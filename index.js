5c7956c322371a6d118da2613bd2326ea766758e
vanCopper <vancopper@foxmail.com>
2020-07-02 23:38:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ function denyRequest (req, res) {
 }
 
 function apiGetServerList(req, res) {
+    loggerInstance.info(`Get server list from: ${req.serverKey}`);
     if(apiIsKeyFromRequestIsBad(req))
     {
         return res.sendStatus(400);
